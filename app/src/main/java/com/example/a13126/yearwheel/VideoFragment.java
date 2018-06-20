6575c6044d61114a2e38123ec644d04a8d7a7a0a
zhoupaopao <1312667058@qq.com>
2018-06-20 11:20:14 +0800

    音乐播放优化

--- app/src/main/java/com/example/a13126/yearwheel/VideoFragment.java
+++ app/src/main/java/com/example/a13126/yearwheel/VideoFragment.java
@@ -3,6 +3,7 @@ package com.example.a13126.yearwheel;
 import android.content.ContentResolver;
 import android.database.Cursor;
 import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
@@ -24,6 +25,7 @@ import com.bumptech.glide.Glide;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.Timer;
@@ -43,6 +45,7 @@ public class VideoFragment extends Fragment {
     TextView tizhong;
     TextView nowxq;
     int j=0;
+    MediaPlayer mediaPlayer=new MediaPlayer();
 
     @Nullable
     @Override
@@ -56,7 +59,7 @@ public class VideoFragment extends Fragment {
         Uri uri=Uri.parse(path);
 //                Log.i("readmsg123: ", arrayList.get(1).split(",")[2]);
 //                Glide.with(getActivity()).load("http://project.thinghigh.cn/uploads/20180608/c18972c8bcf2eaee8c1eb38674477076.jpg").into(img);
-        asyncPlayer.play(getActivity(),uri,true,AudioManager.STREAM_VOICE_CALL);
+//        asyncPlayer.play(getActivity(),uri,true,AudioManager.STREAM_VOICE_CALL);
         play_video=view.findViewById(R.id.show_video);
         img=view.findViewById(R.id.img);
         pci=view.findViewById(R.id.pci);
@@ -69,8 +72,8 @@ public class VideoFragment extends Fragment {
         play_video.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-
+                pci.setVisibility(View.VISIBLE);
+                play_video.setVisibility(View.GONE);
 
 
 //                MediaPlayer.create(this,  Uri.parse("storage/sdcard0/music.mp3"))
@@ -81,6 +84,17 @@ public class VideoFragment extends Fragment {
 //                   nowxq.setText(szitem[1]);
 //                   dateString.setText(szitem[3]);
 //                   Glide.with(getActivity()).load(szitem[2]).into(img);
+
+                    try {
+                        mediaPlayer.setDataSource("/sdcard/Music/123.mp3");
+                        mediaPlayer.setLooping(true);
+                        mediaPlayer.prepare();
+                        mediaPlayer.start();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+
                 final Timer timer=new Timer();
 
                  k=0;
@@ -97,19 +111,37 @@ public class VideoFragment extends Fragment {
                            handler.sendMessage(mm);
 
                            if(k==arrayList.size()-1){
+                               try {
+                                   Thread.sleep(2000);
+                               } catch (InterruptedException e) {
+                                   e.printStackTrace();
+                               }
                                timer.cancel();
+                               mediaPlayer.stop();
+                                //需要到主线程中去进行操作
+                               handler.post(runnable);
+                               //多少秒之后播放
+//                               mediaPlayer.seekTo();
                            }else{
                                Log.i("run: ", "run: ");
                                k++;
                            }
 
                        }
-                   },0,1000);
+                   },0,2000);
 
             }
         });
         return view;
     }
+    Runnable runnable=new Runnable() {
+        @Override
+        public void run() {
+
+            play_video.setVisibility(View.VISIBLE);
+            pci.setVisibility(View.GONE);
+        }
+    };
     Handler handler=new Handler(){
         @Override
         public void handleMessage(Message msg) {
@@ -117,7 +149,7 @@ public class VideoFragment extends Fragment {
                 Bundle dd=msg.getData();
                 String listii=dd.getString("string");
                 String[]szitem=listii.split(",");
-                   tizhong.setText(szitem[0]);
+                   tizhong.setText(szitem[0]+"斤");
                    nowxq.setText(szitem[1]);
                    dateString.setText(szitem[3]);
                    Glide.with(getActivity()).load(szitem[2]).into(img);
@@ -154,5 +186,11 @@ public class VideoFragment extends Fragment {
             Toast.makeText(getActivity(),"找不到指定文件",Toast.LENGTH_SHORT).show();
         }
         return result;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mediaPlayer.release();//释放音频
     }
 }
